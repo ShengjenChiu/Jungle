55ac6a53b5ed3ac1d2f55b38235b7761dc072383
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-30 15:58:45 -0600

    Feature Spec: HomePage-2products on pasge

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,10 +1,12 @@
 describe('products', () => {
 
   it("There is products on the page", () => {
+    cy.visit("/");
     cy.get(".products article").should("be.visible");
   });
 
   it("There is 2 products on the page", () => {
+    cy.visit("/");
     cy.get(".products article").should("have.length", 2);
   });
 
