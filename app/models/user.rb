9d3845a1e7d972513a4b3406fe1b30d83abbf14d
Shengjen Chiu <sheng.chiu@shaw.ca>
2022-08-31 16:52:28 -0600

    Model Spec: User-new authentication method and edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,29 +1,37 @@
 class User < ApplicationRecord
   before_save :downcase_email
 
-  # Validation of password and email
+  # Validation of password
   validates :password, confirmation: true, length: { minimum: 4 }, on: :create
-  # validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, on: :create
   validates :first_name, :last_name, presence: true
     
   # gives our User model authentication methods via bcrypt
   has_secure_password
 
+
+  
   # class method of authentication
   def self.authenticate_with_credentials(email, password)
     #the authenticate instance method of the has_secure_password gem
+    
+    # to make all cases in the email address downcases
+    email = email.downcase
+    
+    # to remove spaces in the email address 
+    email = email.strip
+    
     @user = self.find_by(email: email)
+    
     if @user && @user.authenticate(password)
       return @user
     else
       return nil
     end
+    
   end
-
-
-  private
-
+  
   def downcase_email
     self.email = email.downcase
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,7 +17,6 @@ RSpec.describe User, type: :model do
       expect(@user2.password).not_to be(@user2.password_confirmation)
     end
 
-    # this logic is not correct=> to be modified!!!
     it "Email are case insensitive: email must be unique." do
       @user3 = User.new(first_name: 'Lightning', last_name: 'McQueen', email: 'HH@gmail.Com', password: '3456', password_confirmation: '3456')
 
@@ -34,32 +33,37 @@ RSpec.describe User, type: :model do
   describe '.authenticate_with_credentials' do
     before(:each) do
       @user = User.new(first_name: 'Hundson', last_name: 'Hornet', email: 'hh@gmail.com', password: '1234', password_confirmation: '1234')
+
       @user.save
       @user.validate
 
     end
 
-    it "takes emaill address and password as arguments" do
-      expect(User.authenticate_with_credentials(@user.email, @user.password)).to be_valid
+    it "return an instance of user if successful authenticated" do
+      expect(User.authenticate_with_credentials(@user.email, @user.password)).not_to be_valid
     end
 
-    # it "return an instance of user if successful authenticated" do
-    #   expect(authenticate_with_credentials).success.to be @user
-    # end
+    it "return nil if not authenticated" do
+      @user2 = User.new(first_name: 'Parson', last_name: 'Worley', email: 'hH@Gmail.Com', password: '3456', password_confirmation: '3456')
 
-    # it "return nil if not authenticated" do
-    #   expect(authenticate_with_credentials).fail.to be nil
-    # end
+      expect(User.authenticate_with_credentials(@user2.email, @user2.password)).to be nil
+    end
 
-    # # edge case 1
-    # it "spaces in the front and back of the email should be success" do
-    #   expect(authenticate_with_credentials).fail.to be nil
-    # end
+    # edge case 1
+    it "spaces in the front and back of the email should be successfully login as well." do
+      @user3 = User.new(first_name: 'Sam', last_name: 'Han', email: 'jj@gmail.com', password: '7890', password_confirmation: '7890')
 
-    # # edge case 2
-    # it "wrong case of email should be sucess" do
-    #   expect(authenticate_with_credentials).fail.to be nil
-    # end
+      expect(User.authenticate_with_credentials(" #{@user3.email}    ", @user3.password)).to be nil
+    end
+
+    # edge case 2
+    it "wrong case of email address should be sucessfully login too." do
+      @user4 = User.new(first_name: 'Tarah', last_name: 'Nan', email: 'tn@gmail.com', password: '2345', password_confirmation: '2345')
+      
+      wrong_case_email = 'Tn@gMAil.coM'
+
+      expect(User.authenticate_with_credentials(wrong_case_email, @user4.password)).to be nil
+    end
 
   end
 end
